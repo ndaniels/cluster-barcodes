f51b70ce154bc556417aa5249222190f2c39ffe3
Noah Daniels <ndaniels@mac.com>
2014-02-21 11:38:30 -0500

    Play with not showing singletons. TODO make this command-line option.

--- Main.hs
+++ Main.hs
@@ -17,13 +17,23 @@ main = do
           -- let (goods, bads) = prune $ mkClusters samples
           let clusters = sortBy (comparing numSamples) $ discard $ mkClusters samples
           
-          let edges = map mkGraphEdge $ nearbyClusters clusters
+          -- discard degree-1 nodes, unless they contain >1 samples.
+          let nonSingletons = filter (\c -> degree c (nearbyClusters clusters) > 1 || numSamples c > 1) clusters
+          
+          -- let edgePairs = nearbyClusters clusters
+          
+          let edgePairs = nearbyClusters nonSingletons
+          
+          let edgeStrings = map mkGraphEdge edgePairs
+          
+          -- let nodeStrings = map mkGraphNode clusters
           
           -- let output = (map show clusters) ++ [show $ length clusters]
           
-          let nodes = map mkGraphNode clusters
           
-          let output = buildGraph "Barcode" nodes edges
+          let nodeStrings = map mkGraphNode nonSingletons
+          
+          let output = buildGraph "Barcode" nodeStrings edgeStrings
           
           if outName == "-" 
             then putStrLn output
