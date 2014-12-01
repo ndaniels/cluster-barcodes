a6e0374d18b6a448d1e22d9e46db856766651b8e
Noah Daniels <ndaniels@mac.com>
2014-11-30 23:12:50 -0500

    Some preliminary work for grouped longitudinal graphs. Should now handle foreign newlines.

--- Graph.hs
+++ Graph.hs
@@ -26,3 +26,31 @@ where
                        
   buildGraph :: String -> [String] -> [String] -> String
   buildGraph title nodes edges = mkGraph title (nodes ++ edges)
+  
+  buildLGraph :: String -> [String] -> [String] -> String
+  buildLGraph title nodes edges = mkLGraph title (nodes ++ edges)
+  
+  mkLGraph :: String -> [String] -> String
+  mkLGraph = undefined
+    --
+  -- let nonSingletons = filter (\c -> degree c (dateClusters clusters) >= 1) clusters
+  -- let edgePairs = dateClusters nonSingletons
+  -- let edgeStrings = map mkGraphEdge edgePairs
+  -- let nodeStrings = map mkGraphNode nonSingletons
+  -- let output = buildGraph "Barcodes" nodeStrings edgeStrings
+  --
+  
+  
+  -- mkLSubgraph :: [Cluster] -> String
+  -- mkLSubgraph cs = "subgraph cluster_" ++ (show $ serialNum $ head cs) ++ "{\n"
+  --                 ++ mkLNodeStrings cs
+  --                 ++ mkLEdgeStrings cs
+  --                 ++ "}"
+                  
+  -- mkLNodeStrings
+                  
+  mkLGraphEdge :: (Int,Int) -> String
+  mkLGraphEdge (a,b) = show a ++ " -> " ++ show b ++ ";\n"
+  
+  -- use digraph here, -> edges
+  -- subgraph cluster_1 { nodes and edges }
